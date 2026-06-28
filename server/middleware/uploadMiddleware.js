3581af19249c29871ed85646f942a09b57eeb904
Prithivirajan P <prithivirajan2007@gmail.com>
2026-06-28 21:02:33 +0530

    Refactor upload middleware to use memory storage

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -1,31 +1,12 @@
-import fs from "fs";
 import multer from "multer";
-import path from "path";
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-  const uploadPath = "uploads/";
-
-  if (!fs.existsSync(uploadPath)) {
-    fs.mkdirSync(uploadPath, { recursive: true });
-  }
-
-  cb(null, uploadPath);
-},
-
-  filename: (req, file, cb) => {
-    const uniqueName =
-      Date.now() + path.extname(file.originalname);
-
-    cb(null, uniqueName);
-  },
-});
+const storage = multer.memoryStorage();
 
 const fileFilter = (req, file, cb) => {
   if (file.mimetype.startsWith("image")) {
     cb(null, true);
   } else {
-    cb(new Error("Only image files are allowed."));
+    cb(new Error("Only image files are allowed."), false);
   }
 };
 
